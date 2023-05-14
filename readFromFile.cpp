8871464f68d1f325eef6474b2fe087174ce3ef3c
saraadel6 <101582885+saraadel6@users.noreply.github.com>
2023-05-14 10:58:53 +0300

    Update file readFromFile

--- readFromFile.cpp
+++ readFromFile.cpp
@@ -1,8 +1,9 @@
 #include<bits/stdc++.h>
-#include"studentClass.cpp"
+#include "studentClass.cpp"
+
 using namespace std;
 
-void readFromFile(){
+vector<Student> readFromFile(){
     int size;
     fstream inputFile;
     char file_name[100]="studentData";
@@ -10,22 +11,20 @@ void readFromFile(){
     inputFile.open(file_name,ios::in);
     if(!inputFile.is_open()){
         cout << "ERROR: Could not open file\n";
-        return;
     }
-    inputFile >> size;
-    cout << size << '\n';
-    vector<StudentHeap> sHeap;
-    for(int i = 0; i < size && inputFile.good(); ++i){
-        StudentHeap currStudent;
-        inputFile >> currStudent.ID;
-        getline(inputFile >> ws, currStudent.Name, '\n');
-        inputFile >> currStudent.GPA;
-        getline(inputFile >> ws, currStudent.Department, '\n');
-        sHeap.push_back(currStudent);
-        cout << currStudent.ID << '\n';
-        cout << currStudent.Name << '\n';
-        cout << currStudent.GPA << '\n';
-        cout << currStudent.Department << '\n';
+    else{
+        inputFile >> size;
+        vector<Student> student;
+        for(int i = 0; i < size && inputFile.good(); ++i){
+            Student currStudent;
+            inputFile >> currStudent.ID;
+            getline(inputFile >> ws, currStudent.Name, '\n');
+            inputFile >> currStudent.GPA;
+            getline(inputFile >> ws, currStudent.Department, '\n');
+            student.push_back(currStudent);
+        }
+        inputFile.close();
+        return student;
     }
-    inputFile.close();
+
 }
