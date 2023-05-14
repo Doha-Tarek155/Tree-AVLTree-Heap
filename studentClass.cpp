499c7742a4ed53e4aec016edd198f6fd4b981b44
saraadel6 <101582885+saraadel6@users.noreply.github.com>
2023-05-14 10:59:24 +0300

    update class Student

--- studentClass.cpp
+++ studentClass.cpp
@@ -1,18 +1,43 @@
 #include<bits/stdc++.h>
-using namespace std;
 
-class StudentHeap{
+using namespace std;
+class Student{
 public:
     int ID;
     string Name;
     double GPA;
     string Department;
-    StudentHeap(int ID, string Name, double GPA, string Department);
-    StudentHeap(){}
+    Student(){}
+    Student(int ID, string Name, double GPA, string Department){
+        this->ID = ID;
+        this->Name = Name;
+        this->GPA = GPA;
+        this->Department = Department;
+    };
+    int getID();
+    string getName();
+    double getGPA();
+    string getDepartment();
 };
-StudentHeap::StudentHeap(int ID, string Name, double GPA, string Department){
-    this->ID = ID;
-    this->Name = Name;
-    this->GPA = GPA;
-    this->Department = Department;
+
+//Student::Student(int ID, string Name, double GPA, string Department){
+//    this->ID = ID;
+//    this->Name = Name;
+//    this->GPA = GPA;
+//    this->Department = Department;
+//}
+int Student::getID() {
+    return ID;
+}
+
+string Student::getName() {
+    return Name;
+}
+
+double Student::getGPA() {
+    return GPA;
+}
+
+string Student::getDepartment() {
+    return Department;
 }
